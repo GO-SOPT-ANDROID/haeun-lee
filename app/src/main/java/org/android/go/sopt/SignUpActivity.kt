a145fef37029be91a5743f5f90bf0c13673bf769
leeeha <jxlhe46@seoultech.ac.kr>
2023-04-05 17:16:53 +0900

    feat(회원가입 화면): id, pw 길이가 제한 조건에 맞지 않으면 경고 문구 표시

--- app/src/main/java/org/android/go/sopt/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/SignUpActivity.kt
@@ -2,6 +2,9 @@ package org.android.go.sopt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import org.android.go.sopt.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -11,5 +14,52 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.etId.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!checkValidityOfId(s.toString())){
+                    binding.tvIdLimitError.visibility = View.VISIBLE
+                }else{
+                    binding.tvIdLimitError.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        binding.etPw.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!checkValidityOfPw(s.toString())){
+                    binding.tvPwLimitError.visibility = View.VISIBLE
+                }else{
+                    binding.tvPwLimitError.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        binding.btnSignUp.setOnClickListener {
+            val id = binding.etId.text.toString()
+            val pw = binding.etPw.text.toString()
+            val name = binding.etName.text.toString()
+            val hobby = binding.etHobby.text.toString()
+        }
+    }
+
+    private fun checkValidityOfId(id: String): Boolean {
+        return id.length in 6..10
+    }
+
+    private fun checkValidityOfPw(pw: String): Boolean {
+        return pw.length in 8..12
     }
 }
