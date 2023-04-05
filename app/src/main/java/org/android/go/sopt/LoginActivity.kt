1ace45748387e9cd18c3d08e73375ddda9afa74a
leeeha <jxlhe46@seoultech.ac.kr>
2023-04-05 15:01:31 +0900

    feat: 회원가입 화면으로부터 결과 값을 받아오는 코드 작성

--- app/src/main/java/org/android/go/sopt/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/LoginActivity.kt
@@ -1,15 +1,42 @@
 package org.android.go.sopt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import org.android.go.sopt.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intent = result.data
+                val id = intent?.getStringExtra("id")
+                val pw = intent?.getStringExtra("pw")
+                val name = intent?.getStringExtra("name")
+                val hobby = intent?.getStringExtra("hobby")
+                Log.d(TAG, "$id $pw $name $hobby")
+            }
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+    }
+
+    companion object {
+        private const val TAG = "LoginActivity"
     }
 }
