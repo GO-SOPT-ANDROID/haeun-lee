51e172040810a5cfb483151f5f039e1447b72aa6
leeeha <jxlhe46@seoultech.ac.kr>
2023-04-07 20:13:30 +0900

    remove: 사용하지 않는 Serializable 삭제

--- app/src/main/java/org/android/go/sopt/User.kt
+++ app/src/main/java/org/android/go/sopt/User.kt
@@ -1,10 +1,8 @@
 package org.android.go.sopt
 
-import java.io.Serializable
-
 data class User(
     val id: String,
     val pw: String,
     val name: String,
     val hobby: String
-): Serializable
+)
