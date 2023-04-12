d8c089d70a316056afb53b123280dff23bd26d68
leeeha <jxlhe46@seoultech.ac.kr>
2023-04-12 17:32:38 +0900

    feat: 리사이클러뷰 상단에 타이틀 헤더 추가

--- app/src/main/java/org/android/go/sopt/ui/main/adapter/data/RepoSource.kt
+++ app/src/main/java/org/android/go/sopt/ui/main/adapter/data/RepoSource.kt
@@ -6,11 +6,18 @@ import org.android.go.sopt.ui.main.adapter.model.Repo
 class RepoSource {
     fun loadRepoSources(): List<Repo> {
         return listOf(
-            Repo(R.drawable.ic_launcher_background, "name1", "author1"),
-            Repo(R.drawable.ic_launcher_background, "name2", "author2"),
-            Repo(R.drawable.ic_launcher_background, "name3", "author3"),
-            Repo(R.drawable.ic_launcher_background, "name4", "author4"),
-            Repo(R.drawable.ic_launcher_background, "name5", "author5"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
+            Repo(R.drawable.ic_launcher_background, "name", "author"),
         )
     }
 }
