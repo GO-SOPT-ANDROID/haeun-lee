2e791c005be22cb2499c8ce1d49810bfba6b5a40
leeeha <jxlhe46@seoultech.ac.kr>
2023-05-06 16:23:41 +0900

    [FEAT/#7] SearchFragment에 뷰페이저 적용

--- app/src/main/java/org/android/go/sopt/ui/main/search/SearchFragment.kt
+++ app/src/main/java/org/android/go/sopt/ui/main/search/SearchFragment.kt
@@ -5,11 +5,22 @@ import android.view.View
 import org.android.go.sopt.R
 import org.android.go.sopt.binding.BindingFragment
 import org.android.go.sopt.databinding.FragmentSearchBinding
+import org.android.go.sopt.ui.main.search.adapter.SearchAdapter
 
-/** 리사이클러뷰 Selection 라이브러리 사용해보기 */
+/** Week2: 리사이클러뷰 Selection 라이브러리 사용해보기
+ *  Week4: 뷰페이저 적용하기
+ * */
 class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.vpSearch.adapter = SearchAdapter(
+            listOf(
+                R.drawable.profile_image,
+                R.drawable.profile_image,
+                R.drawable.profile_image,
+                R.drawable.profile_image,
+            )
+        )
     }
 }
