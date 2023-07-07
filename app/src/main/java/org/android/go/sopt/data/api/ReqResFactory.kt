21723900fdd01ab41fcd541732aa142281c84c83
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-07 22:53:28 +0900

    [REMOVE] 불필요한 어노테이션 삭제

--- app/src/main/java/org/android/go/sopt/data/api/ReqResFactory.kt
+++ app/src/main/java/org/android/go/sopt/data/api/ReqResFactory.kt
@@ -1,7 +1,6 @@
 package org.android.go.sopt.data.api
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
@@ -21,7 +20,6 @@ object ReqResFactory {
             })
         .build()
 
-    @OptIn(ExperimentalSerializationApi::class)
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(REQRES_BASE_URL)
         .addConverterFactory(Json.asConverterFactory(CONTENT_TYPE.toMediaType()))
